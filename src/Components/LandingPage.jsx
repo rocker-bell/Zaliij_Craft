bc3d567adb5856ed3e37ba6c86a4f42a7a61bf14
rocker_bell8 <rockbell8@gmail.com>
2026-04-15 11:46:10 +0100

    Add Project startup base components

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import "../Styles/LandingPage.css";
+const LandingPage = () => {
+    return (
+        <>
+            <div className="LandingPage_wrapper">
+                <h1>Zeliij Craft Landing Page</h1>
+            </div>
+        
+        </>
+    )
+}
+
+export default LandingPage;
