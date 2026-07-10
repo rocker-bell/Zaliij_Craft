4a1ad7a9cfa8d8311aec54325cccd71e2e97d3b0
rocker bell <66403608+rocker-bell@users.noreply.github.com>
2026-07-10 21:12:46 +0100

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Zaliij_Craft/",
+  base: "/Zellige_Art_Travaux/",
   envDir: "config"
 })
